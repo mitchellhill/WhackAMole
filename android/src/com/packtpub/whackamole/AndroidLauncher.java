09963e9f30fc25ca114b9c9198406094033cb6e2
Mitchell Hill <mitchellhill@hotmail.com>
2016-12-30 17:49:43 -0600

    Android initial commit

---
+++ android/src/com/packtpub/whackamole/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.packtpub.whackamole;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.packtpub.whackamole.WhackAMole;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WhackAMole(), config);
+	}
+}
