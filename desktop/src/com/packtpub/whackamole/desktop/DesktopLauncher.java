747f158033a0c8abbb08882542740961dcbf41d3
Mitchell Hill <mitchellhill@hotmail.com>
2017-01-25 20:28:15 -0600

    adding all files

---
+++ desktop/src/com/packtpub/whackamole/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packtpub.whackamole.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.whackamole.WhackAMole;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WhackAMole(), config);
+	}
+}
---
+++ html/src/com/packtpub/whackamole/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.packtpub.whackamole.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.packtpub.whackamole.WhackAMole;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WhackAMole();
+        }
+}
